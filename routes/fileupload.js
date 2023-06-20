e0235c5d152219cda14e0cc70888d701f6ce9bcc
Dev Patel <103362332+devrp21@users.noreply.github.com>
2023-06-20 14:51:02 +0530

    Delete fileupload.js

--- routes/fileupload.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const routerFileup=express.Router();
-
-const crypto = require("crypto");
-const path = require("path");
-const mongoose = require("mongoose");
-const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
