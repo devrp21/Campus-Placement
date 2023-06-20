7c0b3795021740d61c321fc45cd18b8e4959ce5b
Dev Patel <103362332+devrp21@users.noreply.github.com>
2023-06-20 14:49:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 const path = require('path');
-// const hbs = require('hbs');
+
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 const routerStd = require('./routes/loginStudentRoute')
@@ -12,7 +12,7 @@ const rounterHome=require('./controller/home');
 const studentlogin = require('./model/student');
 const router = require('./routes/route');
 const bcrypt = require("bcrypt");
-// var selected=require('./templates/views/selectedcompany');
+
 const sessions = require('express-session');
 const flash=require('connect-flash');
 
@@ -23,10 +23,10 @@ const template_path = path.join(__dirname, "./templates/views");
 const partials_path = path.join(__dirname, "./templates/partials");
 
 app.use(bodyparser.urlencoded({ extended: false }));
-// hbs.registerPartials(partials_path);
+
 app.set("view engine", "ejs");
 app.set("views", template_path);
-// app.set("views/admin", template_path);
+
 app.use(express.static(public_path));
 app.use(express.static(path.join(partials_path,'javascript')))
 app.use(express.static('images'));
@@ -53,29 +53,8 @@ app.use('/admin',routerAdm);
 app.use('/company',routerCmp);
 app.get('/', rounterHome.counts);
 
-// console.log(selected);
 
 app.listen(port, () => {
     console.log(`Port Started at ${port}`);
 });
 
-
-
-
-// https://www.youtube.com/watch?v=oLuuIgiyxmg
-
-// https://www.youtube.com/watch?v=zoqW6qSjSfI&list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8
-
-// https://www.youtube.com/watch?v=FL8eFtvS6UI
-
-
-// Apply Button click to apply in company
-// any changes happens in that when i reload it gives error sname is not there
-// navebar error in login student route
-
-// pdf upload kar dai e che frontend aavde but e file select thai to upload kai rite thase
-
-// regex remain undone in admin add student and update student
-// on button click alert message problem
-// password bycrypt in update student
-// how to stop duplicating data in mongodb
